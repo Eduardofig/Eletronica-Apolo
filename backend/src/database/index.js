3951d3df041ba65c2473be5906d51af371e8e6f5
Eduardofig <duzinho039@pop-os.localdomain>
2020-12-11 20:09:23 -0300

    :card_file_box: DATABASE: Conexao com o MongoAtlas
    Criacao do Cluster "Eletronica Apollo", com usuario = "admin" e senha = "pivete", url_de_conexao = "mongodb+srv://admin:pivete@eletronica-apollo.63ug4.mongodb.net/eletronica-apollo?retryWrites=true&w=majority"

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -2,11 +2,11 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost/eletronica_apolo_web", {
+mongoose.connect("mongodb+srv://admin:pivete@eletronica-apollo.63ug4.mongodb.net/eletronica-apollo?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false },
-  ()=>console.log("Conectado ao MongoDB"));
+  ()=>console.log("Conectado ao MongoAtlas"));
 
 module.exports = mongoose;
